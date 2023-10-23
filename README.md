06282852041809b703060263d91da7c3e4fc97b3
ROSENDO MENDOZA <rosendo.arturo@gmail.com>
2023-10-23 19:37:20 +0200

    Day 38: Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is this project?
+This app scrapes a web page and extract data information
---
+++ main.py
@@ -0,0 +1,5 @@
+import requests
+import selectors
+
+URL ="https://programmer100.pythonanywhere.com/tours/"
+
