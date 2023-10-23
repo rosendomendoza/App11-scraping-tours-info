a8de2776712716faa78a4f6b7bc08044813c5bcd
ROSENDO MENDOZA <rosendo.arturo@gmail.com>
2023-10-23 19:58:24 +0200

    Day 38: Define a scrape function

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import requests
-import selectors
+import selectorlib
+
 
 URL ="https://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+                  '39.0.2171.95 Safari/537.36'}
 
+def scrapes (url):
+    response = requests.get(url=url, headers=HEADERS)
+    source = response.text
+    return source
+
+
+if __name__ == "__main__":
+    print(scrapes(URL))
