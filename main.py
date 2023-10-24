d2092f107bd8e724293ed1a93b2274253291bcc7
ROSENDO MENDOZA <rosendo.arturo@gmail.com>
2023-10-24 18:45:27 +0200

    Day 39: Improve using sqlite database

--- main.py
+++ main.py
@@ -1,24 +1,75 @@
+import sqlite3
+import time
 import requests
 import selectorlib
+from my_tools import send_email
 
-
-URL ="https://programmer100.pythonanywhere.com/tours/"
+URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
                   '39.0.2171.95 Safari/537.36'}
 
-def scrapes (url):
+
+def scrapes(url):
     response = requests.get(url=url, headers=HEADERS)
     source = response.text
     return source
 
-def extract (source):
+
+def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(source)['tours']
     return value
 
+
+def store(extracted):
+    cursor = connection.cursor()
+    elem = extracted.split(",")
+    band, city, date = [elem[0].strip(), elem[1].strip(), elem[2].strip()]
+    cursor.execute("INSERT INTO events VALUES(?,?,?)",
+                   (band, city, date))
+    connection.commit()
+
+
+def read():
+    with open("data.txt", "r") as file:
+        content = file.read()
+    return content
+
+
+def readDB(extracted):
+    cursor = connection.cursor()
+    row = extracted.split(",")
+    row = [elem.strip() for elem in row]
+    band, city, date = row
+    cursor.execute("SELECT * FROM events WHERE band=? AND city =? AND "
+                   "date =?", (band, city, date))
+    row = cursor.fetchall()
+    return row
+
+
 if __name__ == "__main__":
-    scraped = scrapes(URL)
-    extracted = extract(scraped)
-    print(extracted)
+    connection = sqlite3.connect("data.db")
+    while True:
+        scraped = scrapes(URL)
+        extracted = extract(scraped)
+
+        if extracted != "No upcoming tours":
+            if not readDB(extracted):
+                store(extracted)
+                message = f"""\
+Subject: New Event
+    
+From: Scrapping APP
+    
+{extracted}
+"""
+                message = message.encode('utf-8')
+                send_email(message)
+                print(f"Evento: {extracted} INCLUIDO")
+            else:
+                print(f"Evento: {extracted} REPETIDO")
+        else:
+            print("No upcoming tours")
+        time.sleep(2)
